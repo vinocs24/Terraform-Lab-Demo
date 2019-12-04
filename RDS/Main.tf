8c123f53c45a3bb7f1e6975e836037b8181b283a
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-04 17:33:56 +0530

    Update Main.tf

--- RDS/Main.tf
+++ RDS/Main.tf
@@ -1,3 +1,25 @@
+# Subnets
+
+resource "aws_subnet" "wp-public-tf" {
+    vpc_id            = aws_vpc.default.id
+    cidr_block        = var.public_subnet_cidr_block
+    availability_zone = "us-west-2a"
+
+    tags = {
+       Name = "wp-public-tf"
+    }
+}
+
+resource "aws_subnet" "wp-private-tf" {
+    vpc_id            = aws_vpc.default.id
+    cidr_block        = var.private_subnet_cidr_block
+    availability_zone = "us-west-2b"
+
+    tags = {
+       Name = "wp-private-tf"
+    }
+}
+
 resource "aws_db_subnet_group" "default" {
     name        = "wp-db-subnet-tf"
     description = "VPC Subnets"
