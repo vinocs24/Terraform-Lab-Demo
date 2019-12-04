9893d1ed1becd5d81fc7baf7b4912efa867732f5
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-04 15:53:56 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,1 +1,13 @@
+# Terraform state will be stored in S3
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-vino1234"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
 
+# Use AWS Terraform provider
+provider "aws" {
+  region = "us-west-2"
+}
