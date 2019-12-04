9661a54cd926b7178b5f6d6481f1d95a8e9638cb
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-04 17:44:34 +0530

    Update Main.tf

--- RDS/Main.tf
+++ RDS/Main.tf
@@ -77,7 +77,7 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_db_instance" "wordpress" {
-    identifier             = "wordpress-tf"
+    identifier             = "wordpress-db-tf"
     allocated_storage      = 5
     engine                 = "mysql"
     engine_version         = "5.7.22"
