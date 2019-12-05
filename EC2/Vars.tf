23c8a9d5c1e342c47292f6be30f17ef525285eea
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-05 14:44:38 +0530

    Update Vars.tf

--- EC2/Vars.tf
+++ EC2/Vars.tf
@@ -37,8 +37,3 @@ variable "private_subnet_cidr_block" {
   description = "Private Subnet"
   default     = "10.1.2.0/24"
 }
-
-variable "availability_zones" {
-  description = "Availability Zones"
-  default     = "us-west-2a,us-west-2b,us-west-2c"
-}
