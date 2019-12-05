e71d9e67d0474d2c52af2d6e4e9e9144af919323
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-05 15:13:14 +0530

    Update VPC.tf

--- RDS/VPC.tf
+++ RDS/VPC.tf
@@ -1,3 +1,23 @@
+# VPC
+
+resource "aws_vpc" "default" {
+    cidr_block = var.vpc_cidr_block
+
+    tags = {
+       Name = "wp-pvc-tf"
+    }
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = aws_vpc.default.id
+
+    tags = {
+       Name = "wp-igw-tf"
+    }
+}
+
 # Subnets
 
 resource "aws_subnet" "wp-public-tf" {
@@ -17,5 +37,20 @@ resource "aws_subnet" "wp-private-tf" {
 
     tags = {
        Name = "wp-private-tf"
+    }
+}
+
+# Route Tables
+
+resource "aws_route_table" "wp-rt-public-tf" {
+    vpc_id = aws_vpc.default.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.default.id
+    }
+
+    tags = {
+       Name = "wp-rt-public-tf"
     }
 }
